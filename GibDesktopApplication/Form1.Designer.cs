973b035fc731eca91e1116780352ded850284636
eyyub.kandemir <eyyub.kandemir@arvento.com>
2025-08-06 17:43:23 +0300

    initial commit

---
+++ GibDesktopApplication/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace GibDesktopApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firmCodeLabel = new System.Windows.Forms.Label();
+            this.firmPassword = new System.Windows.Forms.Label();
+            this.firmCodeTextBox = new System.Windows.Forms.TextBox();
+            this.firmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.loadXmlButton = new System.Windows.Forms.Button();
+            this.getCertificateInfosBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // firmCodeLabel
+            // 
+            this.firmCodeLabel.AutoSize = true;
+            this.firmCodeLabel.Location = new System.Drawing.Point(6, 27);
+            this.firmCodeLabel.Name = "firmCodeLabel";
+            this.firmCodeLabel.Size = new System.Drawing.Size(69, 13);
+            this.firmCodeLabel.TabIndex = 0;
+            this.firmCodeLabel.Text = "Firma Kodu : ";
+            // 
+            // firmPassword
+            // 
+            this.firmPassword.AutoSize = true;
+            this.firmPassword.Location = new System.Drawing.Point(6, 51);
+            this.firmPassword.Name = "firmPassword";
+            this.firmPassword.Size = new System.Drawing.Size(72, 13);
+            this.firmPassword.TabIndex = 1;
+            this.firmPassword.Text = "Firma Şifresi : ";
+            // 
+            // firmCodeTextBox
+            // 
+            this.firmCodeTextBox.Location = new System.Drawing.Point(81, 24);
+            this.firmCodeTextBox.Name = "firmCodeTextBox";
+            this.firmCodeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firmCodeTextBox.TabIndex = 2;
+            // 
+            // firmPasswordTextBox
+            // 
+            this.firmPasswordTextBox.Location = new System.Drawing.Point(81, 48);
+            this.firmPasswordTextBox.Name = "firmPasswordTextBox";
+            this.firmPasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.firmPasswordTextBox.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.firmCodeLabel);
+            this.groupBox1.Controls.Add(this.firmPasswordTextBox);
+            this.groupBox1.Controls.Add(this.firmPassword);
+            this.groupBox1.Controls.Add(this.firmCodeTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Firma Bilgileri";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // loadXmlButton
+            // 
+            this.loadXmlButton.Location = new System.Drawing.Point(60, 156);
+            this.loadXmlButton.Name = "loadXmlButton";
+            this.loadXmlButton.Size = new System.Drawing.Size(75, 23);
+            this.loadXmlButton.TabIndex = 5;
+            this.loadXmlButton.Text = "XML Yükle";
+            this.loadXmlButton.UseVisualStyleBackColor = true;
+            this.loadXmlButton.Click += new System.EventHandler(this.loadXmlButton_Click);
+            // 
+            // getCertificateInfosBtn
+            // 
+            this.getCertificateInfosBtn.Location = new System.Drawing.Point(310, 12);
+            this.getCertificateInfosBtn.Name = "getCertificateInfosBtn";
+            this.getCertificateInfosBtn.Size = new System.Drawing.Size(176, 23);
+            this.getCertificateInfosBtn.TabIndex = 6;
+            this.getCertificateInfosBtn.Text = "Sertifika Bilgileri Getir";
+            this.getCertificateInfosBtn.UseVisualStyleBackColor = true;
+            this.getCertificateInfosBtn.Click += new System.EventHandler(this.getCertificateInfosBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.getCertificateInfosBtn);
+            this.Controls.Add(this.loadXmlButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label firmCodeLabel;
+        private System.Windows.Forms.Label firmPassword;
+        private System.Windows.Forms.TextBox firmCodeTextBox;
+        private System.Windows.Forms.TextBox firmPasswordTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button loadXmlButton;
+        private System.Windows.Forms.Button getCertificateInfosBtn;
+    }
+}
+
---
+++ GibDesktopApplication/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using System.Xml;
+using GibDesktopApplication.Managers;
+using tr.gov.tubitak.uekae.esya.api.asn.x509;
+using tr.gov.tubitak.uekae.esya.api.common.crypto;
+using tr.gov.tubitak.uekae.esya.api.common.util;
+using tr.gov.tubitak.uekae.esya.api.smartcard.util;
+using tr.gov.tubitak.uekae.esya.api.xmlsignature;
+using tr.gov.tubitak.uekae.esya.api.xmlsignature.config;
+using tr.gov.tubitak.uekae.esya.api.xmlsignature.document;
+
+namespace GibDesktopApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string loadedXmlFilePath = "";
+
+        private void loadXmlButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // Sadece XML dosyalarını göstersin
+            openFileDialog.Filter = "XML Dosyaları (*.xml)|*.xml";
+            openFileDialog.Title = "XML Dosyası Seç";
+            openFileDialog.Multiselect = false; // Tek dosya seçilsin
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Seçilen dosyanın yolunu al
+                loadedXmlFilePath = openFileDialog.FileName;
+
+                // İstersen dosya yolunu ekrana göster
+                MessageBox.Show("Seçilen dosya: " + loadedXmlFilePath);
+            }
+        }
+
+        private void getCertificateInfosBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(loadedXmlFilePath))
+            {
+                MessageBox.Show("Lütfen önce bir XML dosyası seçin.");
+                return;
+            }
+
+            try
+            {
+                // 1) Lisans kontrol
+                string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+                string licensePath = Path.Combine(baseDir, "lisans", "lisans.xml");
+                if (!File.Exists(licensePath))
+                {
+                    MessageBox.Show($"Lisans dosyası bulunamadı: {licensePath}");
+                    return;
+                }
+                using (FileStream fs = new FileStream(licensePath, FileMode.Open, FileAccess.Read))
+                    LicenseUtil.setLicenseXml(fs);
+
+                // 2) Smartcard & sertifika
+                SmartCardManager scm = SmartCardManager.getInstance();
+                ECertificate cert = scm.getSignatureCertificate(false, false);
+
+                if (cert == null)
+                {
+                    MessageBox.Show("Geçerli bir sertifika bulunamadı!");
+                    return;
+                }
+
+                string pin = "060606"; // test için sabit
+                BaseSigner signer = scm.getSigner(pin, cert);
+               
+                if (signer == null)
+                {
+                    MessageBox.Show("Signer oluşturulamadı!");
+                    return;
+                }
+
+                // 3) XML yükleme
+                XmlDocument sysXml = new XmlDocument();
+                sysXml.PreserveWhitespace = true;
+                sysXml.Load(loadedXmlFilePath);
+                sysXml.Normalize();
+
+                if (sysXml.DocumentElement == null)
+                {
+                    MessageBox.Show("XML geçersiz!");
+                    return;
+                }
+
+                // 4) DOMDocument oluştur
+                Uri xmlUri = new Uri(Path.GetFullPath(loadedXmlFilePath));
+                byte[] xmlBytes;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    sysXml.Save(ms);
+                    xmlBytes = ms.ToArray();
+                }
+
+                // 5) InMemoryDocument oluştur
+                Document xmlDoc = new InMemoryDocument(
+                    xmlBytes,
+                    loadedXmlFilePath,
+                    "application/xml",
+                    "UTF-8"
+                );
+                // 5) Context yükle
+                string configPath = Path.Combine(baseDir, "config", "xmlsignature-config.xml");
+                if (!File.Exists(configPath))
+                {
+                    MessageBox.Show($"Config dosyası bulunamadı: {configPath}");
+                    return;
+                }
+                Context context = new Context();
+                context.Config = new Config(configPath);
+                
+
+                // 6) SignedDocument oluştur, dokümanı ekle
+                SignedDocument signedDoc = new SignedDocument(context);
+                signedDoc.addDocument(xmlDoc);
+
+                // 7) İmza oluştur ve uygula
+                XMLSignature signature = signedDoc.createSignature();
+                signature.addKeyInfo(cert);
+                signature.sign(signer);
+                
+                // 8) Çıktıyı kaydet
+                string outputPath = Path.Combine(
+                   Path.GetDirectoryName(loadedXmlFilePath),
+                   Path.GetFileNameWithoutExtension(loadedXmlFilePath) + "-imzali.xml"
+                );
+
+                using (FileStream fsOut = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    signedDoc.write(fsOut);
+
+                MessageBox.Show("İmzalama başarıyla tamamlandı: " + outputPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Hata: {ex.Message}", "Kritik Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
